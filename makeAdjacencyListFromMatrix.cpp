6efaa431047ddd7772a6ebd31e2d01714ccdde2d
Sajal Pakira <uniquesajal6@gmail.com>
2026-07-14 13:22:42 +0530

    implement insertEdge

--- makeAdjacencyListFromMatrix.cpp
+++ makeAdjacencyListFromMatrix.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
-#include <>
+#include <fstream>
+using namespace std;
+
+struct Node
+{
+    int vertex;
+    Node *next;
+};
+
+Node *adjList[100] = {NULL};
+
+void insertEdge(int i, int j)
+{
+    Node *curr = new Node;
+    curr->vertex = j;
+    curr->next = NULL;
+    if (adjList[i] == NULL)
+    {
+        adjList[i] = curr;
+        return;
+    }
+    Node *temp = adjList[i];
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = curr;
+}
+
+void makeAdjacencyList()
+{
+}
+
+int main()
+{
+}
