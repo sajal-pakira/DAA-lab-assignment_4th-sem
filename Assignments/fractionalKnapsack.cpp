f2eae48d1eaf8349825d436c8973203e89109939
Sajal Pakira <uniquesajal6@gmail.com>
2026-07-15 12:06:44 +0530

    input objects & sort based on their p/w ratio

---
+++ Assignments/fractionalKnapsack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Item
+{
+    int profit;
+    int weight;
+    float ratio;
+};
+
+Item items[100];
+
+void sortItems(int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (items[j].ratio <= items[j + 1].ratio)
+            {
+                Item temp = items[j];
+                items[j] = items[j + 1];
+                items[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of objects -  ";
+    cin >> n;
+    cout << "Enter knapsack capacity -  ";
+    int c;
+    cin >> c;
+    cout << "Enter profit & weight of each object " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> items[i].profit >> items[i].weight;
+        items[i].ratio = (float)items[i].profit / items[i].weight;
+    }
+
+    sortItems(n);
+}
