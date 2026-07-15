e002c52d1313c5dbb5923133ec54d8ebeaaf82a7
Sajal Pakira <uniquesajal6@gmail.com>
2026-07-15 11:26:19 +0530

    Implement push, pop operation in stack

---
+++ Assignments/DFS_iterative.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int graph[100][100];
+int visited[100] = {0};
+int stack[100];
+
+int top = -1;
+
+bool isEmpty()
+{
+    return top == -1;
+}
+
+void push(int val)
+{
+    if (top == 99)
+    {
+        cout << "Stack overflow!!";
+        return;
+    }
+    stack[++top] = val;
+}
+
+int pop()
+{
+    if (isEmpty())
+    {
+        cout << "Stack underflow!!";
+        return -1;
+    }
+    return stack[top--];
+}
+
+void dfsIterative(int v, int n)
+{
+}
+
+int main()
+{
+    int n, start;
+
+    cout << "Enter the number of vertices: ";
+    cin >> n;
+
+    cout << "Enter the adjacency matrix:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        visited[i] = 0;
+
+    cout << "Enter the starting vertex: ";
+    cin >> start;
+
+    dfsIterative(start, n);
+
+    return 0;
+}
